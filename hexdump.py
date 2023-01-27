c12266366419157387801b5fa3e97ad9dbf9aade
Taylor Bushey <tbushey236@gmail.com>
2023-01-26 20:02:56 -0500

    need to add last line

--- hexdump.py
+++ hexdump.py
@@ -2,50 +2,57 @@ import sys
 
 fd = open(sys.argv[1], "rb")
 
-#print(fd.read())
+data = fd.read()
 
-content = fd.read()
+#print(data)
+dataListCopy = []
 
-#print(content[0:16])
+
+for el in data:
+    if hex(el) >= hex(32) and hex(el) <= hex(126):
+        dataListCopy.append(chr(el))
+    else:
+        dataListCopy.append('.')
+
+#print(dataListCopy)
+
+data = list(data)
+
+dataListCopyHex = []
+
+
+
+
+#print(data)
+
+for el in data:
+    dataListCopyHex.append(f'{el:x}'.rjust(2,'0'))
+
 start = 0
 end = 16
-
-while True:
-    print('{:08x}'.format(start), end='  ')
-    leng = len(content[start:end])
-    print(content[start:end].hex())
-    if end > len(content):
-        print('{:08x}'.format(start+leng), end='  ')
-        break
-    start = start + 16
-    end = end + 16 
-# content = fd.read(16).hex()
-
-# print(content)
-
-#content = fd.read(16)
-
-#print(content)
-
-
-
-
-
-
-
-
-
-
-
-
-# for count,byte in enumerate(content):
-#     #print(hex(byte))
-#     if count % 16 == 0:
-#         print('{:08x}'.format(count), end='  ')
-#         print(hex(byte)[0:16])
-    # print(hex(byte)[2:],end=' ')
-    # if count % 16 == 0 and count != 0:
-    #     print('|')
-    
-
-#print(fd.tell()) specifices the offset
+for i in range(len(dataListCopyHex)):
+    l1 = ''
+    l2 = ''
+    if i % 16 == 0:
+        if len(dataListCopyHex) - i > 15:
+            print('{:08x}'.format(i), end='  ')
+            #print(' '.join(dataListCopyHex[i:i+8]), end='  ')
+            print(' '.join(dataListCopyHex[i:i+8]), end='  ')
+            l1 = ' '.join(dataListCopyHex[i:i+8])
+            #print(' '.join(dataListCopyHex[i+9:i+16]),end='  ')
+            print(' '.join(dataListCopyHex[i+8:i+15]),end='  ')
+            l2 = ' '.join(dataListCopyHex[i+8:i+15])
+            #if len(dataListCopyHex) - i > 15:
+            #print('hi')
+            print('|' + ''.join(dataListCopy[i:i+16]) + '|')
+        else:
+            l1 = ' '.join(dataListCopyHex[i:i+8])
+            l2 = ' '.join(dataListCopyHex[i+8:i+15])
+            #length = 
+            #print(len(l1 + l2) + 10)
+            #y = len(l1 + l2)
+            #print(len(y))
+            length = len('{:08x}'.format(i) + '  ' + ' '.join(dataListCopyHex[i:i+8]) + ' '.join(dataListCopyHex[i+8:i+15]))
+            print('{:08x}'.format(i) + '  ' + ' '.join(dataListCopyHex[i:i+8]) + ' '.join(dataListCopyHex[i+8:i+15]),end='')
+            print('|'.rjust(58-length),end='')
+            print(''.join(dataListCopy[i:i+16]) + '|')
