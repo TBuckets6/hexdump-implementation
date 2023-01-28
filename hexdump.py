b7b197e2b30a257e4169f67f171ab8e2fb571f0e
Taylor Bushey <tbushey236@gmail.com>
2023-01-27 19:48:41 -0500

    need to implement functions and main

--- hexdump.py
+++ hexdump.py
@@ -1,15 +1,27 @@
 import sys
 
+# def main():
+#     data = openAndReadFile()
+    
 fd = open(sys.argv[1], "rb")
-
 data = fd.read()
 
-dataListCopy = []
 
+# def openAndReadFile():
+#     fd = open(sys.argv[1], "rb")
+#     data = fd.read()
+#     return data
+
+dataListCopy = []
+#determine if a character is printable ascii
 for el in data:
-    if hex(el) >= hex(32) and hex(el) <= hex(126):
+    #if it is, then append it to the list so that it may be printed
+    if int(hex(el),16) >= int(hex(32),16) and int(hex(el),16) <= int(hex(126),16):
+    #if hex(el) >= 0x20 and hex(el) <= 0x7e:
         dataListCopy.append(chr(el))
+    #else its not printable ascii, so append a period in it's place
     else:
+        #print(hex(el))
         dataListCopy.append('.')
 
 data = list(data)
@@ -24,17 +36,23 @@ for i in range(len(dataListCopyHex)):
     l1 = ''
     l2 = ''
     if i % 16 == 0:
+        prev = ''
         if len(dataListCopyHex) - i > 15:
             print('{:08x}'.format(i), end='  ')
             print(' '.join(dataListCopyHex[i:i+8]), end='  ')
             l1 = ' '.join(dataListCopyHex[i:i+8])
-            print(' '.join(dataListCopyHex[i+8:i+15]),end='  ')
-            l2 = ' '.join(dataListCopyHex[i+8:i+15])
+            print(' '.join(dataListCopyHex[i+8:i+16]),end='  ')
+            l2 = ' '.join(dataListCopyHex[i+8:i+16])
             print('|' + ''.join(dataListCopy[i:i+16]) + '|')
         else:
-            length = len('{:08x}'.format(i) + '  ' + ' '.join(dataListCopyHex[i:i+8]) + ' '.join(dataListCopyHex[i+8:i+15]))
+            prev = '{:08x}'.format(i)
+            length = len('{:08x}'.format(i) + '  ' + ' '.join(dataListCopyHex[i:i+8]) + ' '.join(dataListCopyHex[i+8:i+16]))
             length2 = len(dataListCopyHex[i:i+16])
-            print('{:08x}'.format(i) + '  ' + ' '.join(dataListCopyHex[i:i+8]) + ' '.join(dataListCopyHex[i+8:i+15]),end='')
-            print('|'.rjust(58-length),end='')
+            print('{:08x}'.format(i) + '  ' + ' '.join(dataListCopyHex[i:i+8]) + ' '.join(dataListCopyHex[i+8:i+16]),end='')
+            print('|'.rjust(61-length),end='')
             print(''.join(dataListCopy[i:i+16]) + '|')
-            print('{:08x}'.format(i + length2))
+    if len(dataListCopy) - 1 == i:
+        print('{:08x}'.format(i + 1), end='')
+        
+#if __name__ == "__main__":
+    #main()
